22d815326a5f79cb298eaf6f04b5a0c8959d9c88
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2024-07-02 12:44:51 +0300

    feat: create class Module

---
+++ src/patterns/Module.ts
@@ -0,0 +1,13 @@
+const Module = (() => {
+  const PI = 3.14
+
+  const length = (radius: number) => 2 * PI * radius;
+  const square = (radius: number) => PI * (radius**2);
+
+  return {
+    length: (radius: number) => length(radius),
+    square: (radius: number) => square(radius),
+  };
+})();
+
+export default Module;
