53a1b6619f414dabfa08ba15e2aad74fde6e160a
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2024-07-02 15:14:02 +0300

    style: class Module

--- src/patterns/Module.ts
+++ src/patterns/Module.ts
@@ -2,7 +2,7 @@ const Module = (() => {
   const PI = 3.14
 
   const length = (radius: number) => 2 * PI * radius;
-  const square = (radius: number) => PI * (radius**2);
+  const square = (radius: number) => PI * (radius ** 2);
 
   return {
     length: (radius: number) => length(radius),
