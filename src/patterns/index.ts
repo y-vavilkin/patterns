221cc1190056cf8ede72b1042f89eaf5218ec793
Yaroslav Vavilkin <y.vavilkin@tg.dunice.net>
2024-07-03 09:52:31 +0300

    feat: create index.ts

---
+++ src/patterns/index.ts
@@ -0,0 +1,6 @@
+export { default as Decorator } from './Decorator';
+export { default as Facade } from './Facade';
+export { default as Factory } from './Factory';
+export { default as Module } from './Module';
+export { default as Observer } from './Observer';
+export { default as Singleton } from './Singleton';
